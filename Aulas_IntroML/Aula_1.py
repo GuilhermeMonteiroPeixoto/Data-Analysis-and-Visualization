7379e84136ce9d0ac672d80d0f5618aaddd8c2ba
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2020-07-04 20:08:06 -0300

    A1 ML

--- Aulas_IntroML/Aula_1.py
+++ Aulas_IntroML/Aula_1.py
@@ -11,16 +11,16 @@ y = dados_estudantes['nota_redacao']
 x = dados_estudantes.drop('nota_redacao',axis=1)
 
 from sklearn.model_selection import train_test_split
-x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size = 0.2)
+x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size = 0.5)
 
 from sklearn.ensemble import ExtraTreesClassifier
 
 modelo = ExtraTreesClassifier()
 modelo.fit(x_treino,y_treino)
 
-previsao = modelo.predict(x_teste[0:20])
+previsao = modelo.predict(x_teste[0:60])
 plt.plot(previsao)
-plt.plot(np.array(y_teste[0:20]))
+plt.plot(np.array(y_teste[0:60]))
 plt.show()
 
 
