59143396295643b93bd295ce47c3f778422f0c86
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2020-07-28 22:16:16 -0300

    Update Analise_Basica_Covid_Brazil.py

--- Analise_COVID_Brasil/Analise_Basica_Covid_Brazil.py
+++ Analise_COVID_Brasil/Analise_Basica_Covid_Brazil.py
@@ -2,38 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import folium
 import numpy as np
+import seaborn as sns
 
 url= 'https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-total.csv'
 arquivo = pd.read_csv(url, sep=',')
 arquivo
 
-
 df_brasil = arquivo[['state','totalCases','deaths','recovered','newCases','newDeaths','date']]
-#df_brasil
-
 
 df_brasil = df_brasil.drop([0])
-#df_brasil
-
 
 df_brasil.rename(columns = {'state':'Estado', 'totalCases': 'CasosTotais', 'deaths': 'Mortes', 'recovered': 'Recuperados','newCases':'NovosCasos','newDeaths':'NovasMortes','date':'Data'}, inplace = True)
-#df_brasil
-
 
 df_brasil.style.background_gradient(cmap='BuPu')
 
+sns.jointplot(x=df_brasil['CasosTotais'], y=df_brasil['Mortes'], kind="hex")
 
 df_brasil[['Mortes','Estado']].plot.bar(x='Estado')
 plt.show()
 
-
 df_brasil[['NovosCasos','Estado']].plot.bar(x='Estado')
 plt.show()
 
-
 df_brasil[['NovasMortes','Estado']].plot.bar(x='Estado')
 plt.show()
-
 
 arquivo_json = 'https://raw.githubusercontent.com/luizpedone/municipal-brazilian-geodata/master/data/Brasil.json'
 
@@ -54,7 +46,6 @@ folium.Choropleth(
 folium.LayerControl().add_to(m)
 
 m.save('Mortes_por_COVID19.html')
-
 
 m = folium.Map(location=[-12.0,-49.5], zoom_start=4)
 
@@ -93,7 +84,6 @@ folium.LayerControl().add_to(m)
 m.save('Recuperados.html')
 
 nordeste = df_brasil.query('Estado==("AL","BA","CE","MA","PB","PE","PI","RN","SE")')
-#nordeste
 
 nordeste.style.background_gradient(cmap='OrRd')
 
@@ -104,8 +94,6 @@ plt.show()
 nordeste[['CasosTotais','Estado']].plot.bar(x='Estado')
 plt.title("Nordeste")
 plt.show()
-
-###
 
 sudeste = df_brasil.query('Estado==("SP","MG","RJ","ES")')
 
@@ -118,4 +106,3 @@ plt.show()
 sudeste[['CasosTotais','Estado']].plot.bar(x='Estado')
 plt.title("Sudeste")
 plt.show()
-
