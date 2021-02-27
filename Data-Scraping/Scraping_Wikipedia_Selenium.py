b920c6312661970dbb653b51d9b3bbd8adb2f15b
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-02-27 16:35:19 -0300

    Update Scraping_Wikipedia_Selenium.py

--- Data-Scraping/Scraping_Wikipedia_Selenium.py
+++ Data-Scraping/Scraping_Wikipedia_Selenium.py
@@ -10,27 +10,43 @@ from selenium.webdriver.common.keys import Keys
 #service = ChromeService('path/to/chromedriver')
 #service.creationflags = CREATE_NO_WINDOW
 
+def print_arquivo_texto(string1, string2, string3, string4):
+    file = open('arquivo_saida_scrap.txt','w+')
+    file.write(string4.upper()+'\n\n')
+    file.write('R: '+string1+'\n')
+    file.write(string2+'\n')
+    file.write(string3+'\n')
+    file.close()
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
-print("\n\n")
-informacao = input("Sobre o que você deseja saber? ")
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("https://pt.wikipedia.org/wiki/")
+def leitura():
+    print("Tell me a topic and I'll search on Wikipedia!\n")
+    informacao = input("What do you want to know about? ")
+    return informacao
 
-sleep(1)
-search = driver.find_element_by_xpath('//*[@id="searchInput"]')
-search.send_keys(informacao)
-click = driver.find_element_by_xpath('//*[@id="searchButton"]')
-click.click()
+def main_func():
 
-sleep(1)
-head1 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[1]')
-head2 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[2]')
-head3 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[3]')
+    print("\n\n")
+    informacao = leitura()
 
-print(head1.text)
-print(head2.text)
-print(head3.text)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("https://en.wikipedia.org/wiki/")
 
-driver.quit()
+    sleep(1)
+    search = driver.find_element_by_xpath('//*[@id="searchInput"]')
+    search.send_keys(informacao)
+    click = driver.find_element_by_xpath('//*[@id="searchButton"]')
+    click.click()
+
+    sleep(1)
+    head1 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[1]')
+    head2 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[2]')
+    head3 = driver.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/p[3]')
+
+    print_arquivo_texto(head1.text, head2.text, head3.text, informacao)
+
+    driver.quit()
+
+main_func()
