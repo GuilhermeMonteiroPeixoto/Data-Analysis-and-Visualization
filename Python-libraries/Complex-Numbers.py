9cfc43289529b2414c315510d0789c6eb1c977f5
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-27 00:44:54 -0300

    Update Complex-Numbers.py

--- Python-libraries/Complex-Numbers.py
+++ Python-libraries/Complex-Numbers.py
@@ -5,20 +5,17 @@ import matplotlib.pyplot as plt
 from datetime import date
 date.today()
 
-#Retorne a parte real e a parte imaginária de a
 a = np.array([1+2j, 3+4j, 5+6j])
 real = a.real
 imag = a.imag
 print("real part=", real)
 print("imaginary part=", imag)
 
-#Substitua a parte real de a por 9, a parte imaginária por [5, 7, 9].
 a = np.array([1+2j, 3+4j, 5+6j])
 a.real = 9
 a.imag = [5, 7, 9]
 print(a)
 
-#Retorne o conjugado complexo de a
 a = 1+2j
 output = np.conjugate(a)
 print(output)
