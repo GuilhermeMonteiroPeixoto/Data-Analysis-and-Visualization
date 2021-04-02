b5b25b810686118d7312bff80d9772a16086f321
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-04-01 22:14:28 -0300

    Update Plotar.py

--- Python-libraries/Speedtest/Plotar.py
+++ Python-libraries/Speedtest/Plotar.py
@@ -5,4 +5,10 @@ arquivo = pd.read_csv('tabela.csv')
 
 plt.plot(arquivo.Download)
 plt.plot(arquivo.Upload)
+plt.legend(["Download", "Upload"], loc =0)
+
+plt.title('Média Download: {} e Média Upload: {}'.format(round(arquivo['Download'].mean(),2), round(arquivo['Upload'].mean(),2)))
+plt.xlabel('time (min)',fontsize=14)
+plt.ylabel('Mbps',fontsize=14)
+
 plt.show()
